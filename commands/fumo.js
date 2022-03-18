57c4cb70db8b359f02a2bf1985cfc8c6eb6194f2
hosma <andrei.hent@yahoo.com>
2022-03-18 02:27:32 +0200

    Implement fumo command

--- commands/fumo.js
+++ commands/fumo.js
@@ -1,20 +1,77 @@
+const { MessageEmbed } = require("discord.js");
+const fumoApi = require("fumo-api");
+const config = require("../config.json");
+
 module.exports = {
     description: "ᗜˬᗜ",
-    // arguments: [],
+    arguments: [
+        "id?"
+    ],
+    aliases: [
+        "fumofumo",
+        "ᗜˬᗜ",
+    ],
 
-    hidden: true,
+    execute({ message, commandArguments } = {}) {
+        if (commandArguments[0]) {
+            fumoApi.getFumoByID(commandArguments[0], true)
+                .then((fumo)=> {
+                    return message.reply({
+                        embeds: [
+                            new MessageEmbed({
+                                color: config.color,
 
-    execute({ message } = {}) {
-        // 
+                                title: "Open Image",
+                                url: fumo.URL,
+                                description: "<:cirnofancy:954173256947077140> Your fumo, sir.",
 
-        message.reply({
-            content: "test",
-            embeds: [],
+                                image: {
+                                    url: fumo.URL,
+                                },
+                            })
+                        ],
 
-            // Without pinging
-            allowedMentions: {
-                repliedUser: false
-            }
-        });
+                        // Without pinging
+                        allowedMentions: {
+                            repliedUser: false
+                        }
+                    });
+                }).catch((ERROR) => {
+                    console.error(ERROR);
+                    return message.reply("```arm\n" + ERROR + "```");
+                })
+        }
+
+        else {
+            fumoApi.randomFumo(true)
+                .then((fumo)=> {
+                    return message.reply({
+                        embeds: [
+                            new MessageEmbed({
+                                color: config.color,
+
+                                title: "Open Image",
+                                url: fumo.URL,
+                                description: "<:cirnofancy:954173256947077140> Your fumo, sir.",
+
+                                image: {
+                                    url: fumo.URL,
+                                },
+                                footer: {
+                                    text: "ID: " + fumo._id,
+                                }
+                            })
+                        ],
+
+                        // Without pinging
+                        allowedMentions: {
+                            repliedUser: false
+                        }
+                    });
+                }).catch((ERROR)=> {
+                    console.error(ERROR);
+                    return message.reply("```arm\n"+ERROR+"```");
+                })
+        }
     }
 }
